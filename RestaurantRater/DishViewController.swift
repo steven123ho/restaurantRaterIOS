44b086324b37aab11fe44725d4da98eecb91c6f4
steven123ho <steven123ho@gmail.com>
2024-03-24 12:27:18 -0400

    First Commit

---
+++ RestaurantRater/DishViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DishViewController.swift
+//  RestaurantRater
+//
+//  Created by user246123 on 3/24/24.
+//
+
+import UIKit
+
+class DishViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtDishName: UITextField!
+    @IBOutlet weak var txtDishType: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitDish(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- RestaurantRater/ViewController.swift
+++ RestaurantRater/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var currentRestaurant: Restaurant?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let textFields: [UITextField] = [txtName, txtAddress]
+        
+        for textfield in textFields {
+            textfield.addTarget(self,
+                                action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)),
+                                for: UIControl.Event.editingDidEnd)
+        }
     }
-
-
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if currentRestaurant == nil {
+            let context = appDelegate.persistentContainer.viewContext
+            currentRestaurant = Restaurant(context: context)
+        }
+        currentRestaurant?.name = txtName.text
+        currentRestaurant?.address = txtAddress.text
+        return true
+    }
+    
+    
+    
+    @IBAction func saveRestaurant(_ sender: Any) {
+        appDelegate.saveContext()
+        sgmtEditMode.selectedSegmentIndex = 0
+        changeEditMode(self)
+    }
+    
+    
+    @IBAction func changeEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtAddress]
+        
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            navigationItem.rightBarButtonItem = nil
+        } else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+        }
+    }
+    
 }
 
